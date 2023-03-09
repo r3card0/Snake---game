8169615159072f16fca7e43fa914d4800f47aeb6
Ricardo Saldivar <ricardo.rdz@pm.me>
2023-03-09 16:42:02 -0600

    bloque move capability

--- screen_moving_block.py
+++ screen_moving_block.py
@@ -2,9 +2,9 @@ import pygame
 # import time
 from pygame.locals import *
 
-def bloque():
+def dibujar_bloque():
     window_game.fill((171, 179, 64))
-    window_game.blit(bloque,(0,0))
+    window_game.blit(bloque,(bloque_x,bloque_y))
     pygame.display.flip()
 
 
@@ -16,7 +16,9 @@ if __name__ == "__main__":
 
     # cargar un bloque - image
     bloque = pygame.image.load('img/rect815.png').convert()
-    window_game.blit(bloque,(0,0))
+    bloque_x = 100
+    bloque_y = 100
+    # window_game.blit(bloque,(0,0))
 
     pygame.display.flip() # actualiza la pantalla del juego
     # time.sleep(5)
@@ -28,5 +30,23 @@ if __name__ == "__main__":
             if event.type == KEYDOWN:
                 if event.key == K_ESCAPE:
                     running = False
+                
+                if event.key == K_UP:   # se refiere a la tecla de la flecha hacia arriba
+                    bloque_y -= 10      # que se mueva 10 posiciones a la inicial
+                    dibujar_bloque()    # después del movimiento, que dibuje el bloque
+
+                if event.key == K_DOWN: # se refiere a la tecla de la flecha hacia abajo
+                    bloque_y += 10      # que se mueva 10 posiciones a la inicial
+                    dibujar_bloque()    # después del movimiento, que dibuje el bloque
+
+                if event.key == K_LEFT:
+                    bloque_x -= 10
+                    dibujar_bloque()    # después del movimiento, que dibuje el bloque
+
+                if event.key == K_RIGHT:
+                    bloque_x += 10
+                    dibujar_bloque()    # después del movimiento, que dibuje el bloque
+                    
+
             elif event.type == QUIT:
                 running = False
